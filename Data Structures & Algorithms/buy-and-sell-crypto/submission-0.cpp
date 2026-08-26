c208162da1fb9482c698c8386cbc28ac957ae867
Nitesh Mehta <niteshmehta1741@gmail.com>
2026-08-26 23:32:13 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res = 0, mn = INT_MAX;
+        for(int i = 0; i < prices.size(); i++){
+            res = max(res, prices[i] - mn);
+            mn = min(mn, prices[i]);
+        }
+        return res;
+    }
+};
